9774689871e9facec5784c553c1787acdd6b71e5
Teri <teri.eich@gmail.com>
2019-05-03 14:54:31 +0000

    added solution to kata-03

--- kata-03.js
+++ kata-03.js
@@ -1,7 +1,19 @@
 // Exercise: Count the number of vowels that appear in a given string.
 
 let numberOfVowels = function(data) {
-  // Put your solution here
+  let n = 0;
+  let i = 0;
+  while (i < data.length) {
+    if (data.charAt(i) === "a"
+      || data.charAt(i) === "e"
+      || data.charAt(i) === "i"
+      || data.charAt(i) === "o"
+      || data.charAt(i) === "u") {
+      n++;
+    }
+    i++
+  }
+  return n;
 };
 
 console.log(numberOfVowels("orange"));
