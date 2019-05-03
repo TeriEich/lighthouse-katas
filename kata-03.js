b684b080932b84fe8346771917152597a8a40a95
Teri <teri.eich@gmail.com>
2019-05-03 14:40:55 +0000

    added exercise setup

---
+++ kata-03.js
@@ -0,0 +1,14 @@
+// Exercise: Count the number of vowels that appear in a given string.
+
+let numberOfVowels = function(data) {
+  // Put your solution here
+};
+
+console.log(numberOfVowels("orange"));
+console.log(numberOfVowels("lighthouse labs"));
+console.log(numberOfVowels("aeiou"));
+
+// Expected Output:
+//  3
+//  5
+//  5
