08de042e1367a30994d55c3820a54ac1c003679a
Teri <teri.eich@gmail.com>
2019-05-03 16:04:19 +0000

    added solution to kata-04

--- kata-04.js
+++ kata-04.js
@@ -1,7 +1,14 @@
 // Exercise: determine which instructor has the longest name.
 
 const instructorWithLongestName = function(instructors) {
-  // Put your solution here
+    let longestName = instructors[0];
+  for (let instructor of instructors) {
+    if (instructor["name"].length > longestName["name"].length) {
+      longestName = instructor;
+    }
+  }
+  return longestName;
+
 };
 
 console.log(instructorWithLongestName([
@@ -9,12 +16,12 @@ console.log(instructorWithLongestName([
   {name: "Jeremiah", course: "Web"},
   {name: "Ophilia", course: "Web"},
   {name: "Donald", course: "Web"}
-]);
+]));
 console.log(instructorWithLongestName([
   {name: "Matthew", course: "Web"},
   {name: "David", course: "iOS"},
   {name: "Domascus", course: "Web"}
-]);
+]));
 
 // Expected Output:
 //  {name: "Jeremiah", course: "Web"}
