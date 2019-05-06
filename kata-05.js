3696eec2f792bedc8f2c8066b518967f1d8c4325
Teri <teri.eich@gmail.com>
2019-05-06 12:53:56 +0000

    added setup for kata 5

---
+++ kata-05.js
@@ -0,0 +1,18 @@
+// Exercise: Given a normal string of words,
+// turn it into a percent-encoded string by
+// replacing all whitespace with %20
+
+
+const urlEncode = function(text) {
+
+};
+
+console.log(urlEncode("Lighthouse Labs"));
+console.log(urlEncode(" Lighthouse Labs "));
+console.log(urlEncode("blue is greener than purple for sure"));
+
+
+// Expected Output:
+// Lighthouse%20Labs
+// Lighthouse%20Labs
+// blue%20is%20greener%20than%20purple%20for%20sure
