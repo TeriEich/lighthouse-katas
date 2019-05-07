8c085c71a652957517622a330bae1a5e5dc3f490
Teri <teri.eich@gmail.com>
2019-05-07 14:19:38 +0000

    added solution to kata 7

--- kata-07.js
+++ kata-07.js
@@ -1,9 +1,22 @@
 // Exercise: Convert normal text to camelCase.
 
 let camelCase = function(input) {
-  let str = input.split(" ");
-  return str;
+  let inputCamel = "";
+  for (let i = 0; i < input.length; i++) {
+    let match = input[i].match(/ /gi);
+    if (match) {
+      let nextLetter = input[i+1];
+      inputCamel += nextLetter.toUpperCase();
+      i++;
+    }
+    else if (input[i-1] !== " ") {
+      inputCamel += input[i];
+    }
+
+  }
+  return inputCamel;
 };
+
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
