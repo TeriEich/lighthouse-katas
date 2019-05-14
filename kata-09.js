5d87b098579cc0d78f477dca9c79b182c5dd906b
Teri <teri.eich@gmail.com>
2019-05-14 23:55:39 +0000

    added setup for kata 9

---
+++ kata-09.js
@@ -0,0 +1,15 @@
+// Exercise: Create a function named talkingCalendar that takes in a date string
+// with the format YYYY/MM/DD, and returns a new human readable date that looks
+// like December 2nd, 2017.
+
+let talkingCalendar = function(date) {
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
+
+// Expected Output:
+// December 2nd, 2017
+// November 11th, 2007
+// August 24th, 1987
