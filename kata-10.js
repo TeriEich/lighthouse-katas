af4653ba0a0e784681fb17f5578f3da25888aa0c
Teri <teri.eich@gmail.com>
2019-05-28 17:38:01 +0000

    finished solution to kata 10

--- kata-10.js
+++ kata-10.js
@@ -14,7 +14,47 @@
 // - Penny (1¢)
 
 const calculateChange = function(total, cash) {
-  // Your code here
+  let returnedCash = cash - total;
+  let change = {};
+  while (returnedCash > 0) {
+    if (returnedCash >= 2000) {
+      change.twentyDollar = Math.floor(returnedCash / 2000);
+      returnedCash = returnedCash % 2000;
+    }
+    else if (returnedCash >= 1000) {
+      change.tenDollar = Math.floor(returnedCash / 1000);
+      returnedCash = returnedCash % 1000;
+    }
+    else if (returnedCash >= 500) {
+      change.fiveDollar = Math.floor(returnedCash / 500);
+      returnedCash = returnedCash % 500;
+    }
+    else if (returnedCash >= 200) {
+      change.twoDollar = Math.floor(returnedCash / 200);
+      returnedCash = returnedCash % 200;
+    }
+    else if (returnedCash >= 100) {
+      change.oneDollar = Math.floor(returnedCash / 100);
+      returnedCash = returnedCash % 100;
+    }
+    else if (returnedCash >= 25) {
+      change.quarter = Math.floor(returnedCash / 25);
+      returnedCash = returnedCash % 25;
+    }
+    else if (returnedCash >= 10) {
+      change.dime = Math.floor(returnedCash / 10);
+      returnedCash = returnedCash % 10;
+    }
+    else if (returnedCash >= 5) {
+      change.nickel = Math.floor(returnedCash / 5);
+      returnedCash = returnedCash % 5;
+    }
+    else if (returnedCash >= 1) {
+      change.penny = Math.floor(returnedCash / 1);
+      returnedCash = returnedCash % 1;
+    }
+  }
+  return change;
 };
 
 console.log(calculateChange(1787, 2000));
